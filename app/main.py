2f3cfad5655491abf90852fdb6b5a1a0a2e228f7
ahad <ahadg446@gmail.com>
2025-03-26 13:00:25 +0500

    configuring for railway 🛫

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 from app.routes import users
 
 app = FastAPI(title="Secure Banking System")
@@ -9,3 +11,7 @@ app.include_router(users.router, prefix="/users", tags=["Users"])
 @app.get("/")
 def root():
     return {"message": "Welcome to Secure Banking API"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Railway provides PORT
+    uvicorn.run(app, host="0.0.0.0", port=port)
